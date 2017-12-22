2c389a6f7a68ec118c583c2a4bbe50905a838324
Remy <reemap642@gmail.com>
2017-12-22 10:32:24 -0500

    adding more

--- Sample/src/Application/User.java
+++ Sample/src/Application/User.java
@@ -3,6 +3,7 @@ package Application;
 public class User {
 public void show() {
 	System.out.println("Hello");
+	System.out.println("Helloworld");
 }
 public void start() {
 	System.out.println("now starting");
