6646ac6d7c289f523b2972ec1d2fcbb6ad2a9cbb
remy <reemap642@gmail.com>
2017-12-26 11:28:23 -0500

    add5

--- Sample/src/Application/main2.java
+++ Sample/src/Application/main2.java
@@ -13,6 +13,13 @@ public class main2 {
 
 		
 	}
+	public void  add5() {
+		System.out.println("new mwthod");
+		System.out.println("new mwthod");
+		System.out.println("new mwthod1");
+
+		
+	}
 	public void  add1() {
 		System.out.println("new mwthod");
 		
